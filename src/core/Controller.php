285770ed284e3b400e3195fc180e2733bd8ebbc9
r567tw <r567tw@gmail.com>
2022-08-18 23:42:21 +0800

    add : json from file

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -16,6 +16,12 @@ class Controller
         return json_encode(["data"=> $params]);
     }
 
+    public function jsonFromFile(string $filePath)
+    {
+        $content = file_get_contents($filePath);
+        return json_encode($content);
+    }
+
     public function getMiddlewares()
     {
         return $this->middlewares;
