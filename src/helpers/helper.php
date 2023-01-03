302610444b499d3810c0fe30f0a29a49499f5860
r567tw <r567tw@gmail.com>
2023-01-03 17:56:57 +0800

    add helper

--- src/helpers/helper.php
+++ src/helpers/helper.php
@@ -4,3 +4,9 @@ namespace r567tw\focus\helpers;
  * @author r567tw <r567tw@gmail.com>
  * @package helpers
  */
+if (! function_exists('hello')) {
+    function hello($name = 'helper')
+    {
+        echo "Hello,{$name}";
+    }
+}
